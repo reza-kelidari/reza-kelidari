0d94765c0c7e85fc73712064c4dda3f959bd05aa
Reza Kelidari <re.kelidari@gmail.com>
2024-09-05 02:46:08 +0330

    Some small improvements
    Now scrolling by link is smooth
    Also added some seo details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import { Vazirmatn } from "next/font/google";
 import "./globals.scss";
-import Head from "next/head";
 
 const vazirmatn = Vazirmatn({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "رضا کلیدری",
+  title: "رضا کلیدری | توسعه دهنده فرانت اند",
   description:
-    "رزومه ی شخصی من، شامل مشخصات من، نمونه کار های من، مهارت های من، روش های ارتباط با من، و...",
+    "سلام، من رضا کلیدری هستم و این رزومه ی شخصی من هستش، شامل مشخصات من، نمونه کار های من، مهارت های من، روش های ارتباط با من، و...",
   authors: [{ name: "رضا کلیدری", url: "https://github.com/reza-kelidari" }],
   keywords: [
     "رضا کلیدری",
+    "کلیدری",
+    "رضا",
     "برنامه نویس",
     "front-end",
     "فرانت اند",
@@ -25,6 +26,27 @@ export const metadata: Metadata = {
   other: {
     "google-site-verification": "Z0S9zls7euBvZaDeMnR8X8JxUYKWHjEKiIULDXhTcz8",
   },
+  robots: "all",
+  openGraph: {
+    type: "website",
+    title: "رضا کلیدری | توسعه دهنده فرانت اند",
+    description:
+      "سلام، من رضا کلیدری هستم و این رزومه ی شخصی من هستش، شامل مشخصات من، نمونه کار های من، مهارت های من، روش های ارتباط با من، و...",
+    url: "https://reza-kelidari.github.io/reza-kelidari",
+    images: "https://reza-kelidari.github.io/reza-kelidari/images/logo.png",
+    countryName: "iran",
+    locale: "fa",
+    siteName: "رضا کلیدری | توسعه دهنده فرانت اند",
+  },
+  twitter: {
+    title: "رضا کلیدری | توسعه دهنده فرانت اند",
+    description:
+      "سلام، من رضا کلیدری هستم و این رزومه ی شخصی من هستش، شامل مشخصات من، نمونه کار های من، مهارت های من، روش های ارتباط با من، و...",
+    site: "https://reza-kelidari.github.io/reza-kelidari",
+    creator: "رضا کلیدری",
+    images: "https://reza-kelidari.github.io/reza-kelidari/images/logo.png",
+  },
+  icons: "https://reza-kelidari.github.io/reza-kelidari/favicon.ico",
 };
 
 export default function RootLayout({
