9d3978f33558c8e2e17fd8347b2985a1d70e19fa
Reza Kelidari <re.kelidari@gmail.com>
2024-09-04 18:37:00 +0330

    Adding github actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import styles from "./page.module.scss";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
 
 export default function Home() {
   return (
     <main className={styles.home}>
       <Hero />
+      <About />
     </main>
   );
 }
